b650c5e68e8624e8cf6adb013bff1175bf1371fe
da21nny <edgarv21.ev@gmail.com>
2025-12-31 14:03:49 -0300

    agregamos inicio y fin

--- main.js
+++ main.js
@@ -10,39 +10,72 @@ const TERRENO = {
 
 let tablero = [];
 
-function crear_tablero(){
+function main() {
+    //Botones para generar tablero y enviar coordenadas de inicio y fin.
+    const btnGenerar = document.getElementById('btnGenerar');
+    const btnEnviar = document.getElementById('btnEnviar');
+
+    btnGenerar.addEventListener('click', crear_tablero);
+    btnEnviar.addEventListener('click', coordenadas_inicio_fin);
+}
+
+function crear_tablero(fila, columna) {
     const numFila = parseInt(document.getElementById('fila').value);
-    const numColumna = parseInt(document.getElementById('columna').value)
+    const numColumna = parseInt(document.getElementById('columna').value);
 
     tablero = [];
-    for(let y = 0; y < numFila; y++){
+    for (let y = 0; y < numFila; y++) {
         let fila_actual = [];
-        for(let x = 0; x < numColumna; x++){
+        for (let x = 0; x < numColumna; x++) {
             fila_actual.push(TERRENO.LIBRE);
         }
         tablero.push(fila_actual);
     }
     generar_obstaculos(numFila, numColumna);
-
     mostrar_tablero(numFila, numColumna);
 }
 
-function generar_obstaculos(fila, columna){
+function generar_obstaculos(fila, columna) {
     const numObstaculos = Math.floor((fila * columna) * 0.3);
-    for(let i = 0; i < numObstaculos; i++){
+    for (let i = 0; i < numObstaculos; i++) {
         const valor_fila = Math.floor(Math.random() * fila);
         const valor_columna = Math.floor(Math.random() * columna);
         const tipo_terreno = Math.floor(Math.random() * 3) + 1;
 
         const actual = tablero[valor_fila][valor_columna];
 
-        if(actual !== TERRENO.INICIO && actual !== TERRENO.FIN){
+        if (actual !== TERRENO.INICIO && actual !== TERRENO.FIN) {
             tablero[valor_fila][valor_columna] = tipo_terreno;
         }
     }
 }
 
-function mostrar_tablero(fila, columna){
+function coordenadas_inicio_fin() {
+    const fila = tablero.length;
+    const columna = tablero[0].length;
+
+    const inicio_x = parseInt(document.getElementById('inicial_x').value) - 1;
+    const inicio_y = parseInt(document.getElementById('inicial_y').value) - 1;
+    const fin_x = parseInt(document.getElementById('fin_x').value) - 1;
+    const fin_y = parseInt(document.getElementById('fin_y').value) - 1;
+
+    if (inicio_x < 0 || inicio_x >= fila || inicio_y < 0 || inicio_y >= columna) {
+        alert('Coordenadas de inicio invalidas');
+        return 1;
+    }
+
+    if (fin_x < 0 || fin_x >= fila || fin_y < 0 || fin_y >= columna) {
+        alert('Coordenadas de fin invalidas');
+        return 1;
+    }
+
+    tablero[inicio_y][inicio_x] = TERRENO.INICIO;
+    tablero[fin_y][fin_x] = TERRENO.FIN;
+
+    mostrar_tablero(fila, columna);
+}
+
+function mostrar_tablero(fila, columna) {
     const contenedorID = document.getElementById('mapaVisual');
 
     contenedorID.innerHTML = '';
@@ -50,17 +83,16 @@ function mostrar_tablero(fila, columna){
     contenedorID.style.gridTemplateColumns = `repeat(${columna}, 35px)`;
     contenedorID.style.gridTemplateRows = `repeat(${fila}, 35px)`;
 
-    for(let y = 0; y < fila; y++){
-        for(let x = 0; x < columna; x++){
+    for (let y = 0; y < fila; y++) {
+        for (let x = 0; x < columna; x++) {
             const celdaDiv = document.createElement('div');
             celdaDiv.classList.add('cell'); // Añadir clase común a todas las celdas
             celdaDiv.dataset.fila = y; // Almacenar fila en dataset
             celdaDiv.dataset.columna = x; // Almacenar columna en dataset
-            celdaDiv.style.cursor = "pointer"; // Indica que es clickable
 
             const valor = tablero[y][x]; // Obtener el valor del terreno en la celda
 
-            switch(valor){ // Asignar clase y contenido según el tipo de terreno
+            switch (valor) { // Asignar clase y contenido según el tipo de terreno
                 case TERRENO.EDIFICIO: // Edificio
                     celdaDiv.textContent = 'X';
                     celdaDiv.classList.add('edificio');
@@ -72,7 +104,7 @@ function mostrar_tablero(fila, columna){
                 case TERRENO.BLOQUEO: // Bloqueo
                     celdaDiv.textContent = 'B';
                     celdaDiv.classList.add('bloqueo');
-                    break; 
+                    break;
                 case TERRENO.INICIO: // Inicio
                     celdaDiv.textContent = 'E';
                     celdaDiv.classList.add('entrada');
@@ -81,7 +113,7 @@ function mostrar_tablero(fila, columna){
                     celdaDiv.textContent = 'S';
                     celdaDiv.classList.add('salida');
                     break;
-                case TERRENO.CAMINO: 
+                case TERRENO.CAMINO:
                     celdaDiv.textContent = '*';
                     celdaDiv.classList.add('camino');
                     break;
@@ -93,11 +125,6 @@ function mostrar_tablero(fila, columna){
             contenedorID.appendChild(celdaDiv);
         }
     }
-}
-
-function main(){
-    const btnGenerar = document.getElementById('btnGenerar');
-    btnGenerar.addEventListener('click', crear_tablero);
 }
 
 document.addEventListener('DOMContentLoaded', main);
